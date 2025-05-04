784408ffb99bea81268fb4a16e5494c38b684677
AJSafeguardSolutions <anto.safetyguardian@gmail.com>
2025-05-04 22:41:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // AJ Safeguard Solutions
+  botOwner: 'AJ Safeguard Solutions', // Your Name
   ownerNumber: '919486606458', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
